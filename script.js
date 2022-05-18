eebe8ffe22f13fe7576b7e454731e96a0d8c0583
Vladislav <vladfyklev@gmail.com>
2022-05-18 15:46:36 +0300

    добавил счетчик товаров

--- script.js
+++ script.js
@@ -62,3 +62,70 @@ $(".slider").slick({
 })
 
 
+// 
+// const decrementBtn = document.querySelectorAll(".decrement-btn");
+// const incrementBtn = document.querySelectorAll(".increment-btn");
+// const quantity = document.querySelectorAll(".product-quantity input");
+
+
+
+// const currValue = +quantity.value;
+// toggleButton(currValue)
+
+// function toggleButton(count) {
+//     decrementBtn.disabled = count <=1;
+//     incrementBtn.disabled = count >=5;
+// }
+
+// function toggleButtonState(count, incrementBtn, decrementBtn) {
+//     decrementBtn.disabled = count <= 1;
+//     incrementBtn.disabled = count >= 5;
+//   }
+// incrementBtn.forEach((incrementBtns,i)=>{
+//     incrementBtns.addEventListener("click" , function () {
+//         const nextValue = +quantity[i].value + 1;
+//         quantity[i].value = nextValue
+//         toggleButton(nextValue)
+//     })
+// })
+
+// decrementBtn.forEach((decrementBtns,i) => {
+//     decrementBtns.addEventListener("click" , function () {
+//         const nextValue = +quantity[i].value - 1;
+//         quantity[i].value = nextValue
+//         toggleButton(nextValue)
+//     })
+// })
+
+const decrementBtns = document.querySelectorAll(".decrement-btn");
+const incrementBtns = document.querySelectorAll(".increment-btn");
+const quantityYnputs = document.querySelectorAll(".product-quantity input");
+function Counter(incrementBtn,decrementBtn,inputField, minCount = 1, maxCount = 5) {
+    this.domRefs= {
+        incrementBtn,
+        decrementBtn,
+        inputField,
+    }
+    this.toggleButtonState = function () {
+        const count = this.domRefs.inputField.value;
+        this.domRefs.decrementBtn.disabled = count <= minCount;
+        this.domRefs.incrementBtn.disabled = count >= maxCount;
+    }
+    this.toggleButtonState()
+
+    this.increment = function () {
+        this.domRefs.inputField.value = +this.domRefs.inputField.value + 1
+        this.toggleButtonState()
+    }
+    this.decrement = function () {
+        this.domRefs.inputField.value = +this.domRefs.inputField.value - 1
+        this.toggleButtonState()
+    }
+   
+    this.domRefs.incrementBtn.addEventListener("click", this.increment.bind(this))
+    this.domRefs.decrementBtn.addEventListener("click", this.decrement.bind(this))
+}   
+
+const counter1 = new Counter(incrementBtns[0],decrementBtns[0], quantityYnputs[0])
+
+console.log(counter1);
